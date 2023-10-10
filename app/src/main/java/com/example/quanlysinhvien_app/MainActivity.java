f14a6c6721da8e035eb892d6e2774c714918e06c
4nh <tranhanhle.2003@gmail.com>
2023-10-10 21:03:17 +0700

    suagiaodien

--- app/src/main/java/com/example/quanlysinhvien_app/MainActivity.java
+++ app/src/main/java/com/example/quanlysinhvien_app/MainActivity.java
@@ -20,32 +20,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nhapthongtinsv);
 
-        // Khởi tạo DatabaseHelper khi mở MainActivity
-        mDatabaseHelper = new DatabaseHelper(this);
-
-        // Mở cơ sở dữ liệu để đọc hoặc ghi
-        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
-        // Hoặc sử dụng để đọc dữ liệu: SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
-
-        // Thực hiện các thao tác với cơ sở dữ liệu ở đây
-        //...
-
-        // Đừng quên đóng cơ sở dữ liệu khi bạn đã sử dụng xong
-
-
-        // Tìm LinearLayout theo id
-        LinearLayout btnCaiDat = findViewById(R.id.btn_caidat);
-
-        // Đặt OnClickListener cho LinearLayout
-        btnCaiDat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Chuyển sang CaidatActivity khi nhấn vào btn_caidat
-                Intent intent = new Intent(MainActivity.this, CaidatActivity.class);
-                startActivity(intent);
-            }
-        });
+//        // Khởi tạo DatabaseHelper khi mở MainActivity
+//        mDatabaseHelper = new DatabaseHelper(this);
+//
+//        // Mở cơ sở dữ liệu để đọc hoặc ghi
+//        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+//        // Hoặc sử dụng để đọc dữ liệu: SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
+//
+//        // Thực hiện các thao tác với cơ sở dữ liệu ở đây
+//        //...
+//
+//        // Đừng quên đóng cơ sở dữ liệu khi bạn đã sử dụng xong
+//
+//
+//        // Tìm LinearLayout theo id
+//        LinearLayout btnCaiDat = findViewById(R.id.btn_caidat);
+//
+//        // Đặt OnClickListener cho LinearLayout
+//        btnCaiDat.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Chuyển sang CaidatActivity khi nhấn vào btn_caidat
+//                Intent intent = new Intent(MainActivity.this, CaidatActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
