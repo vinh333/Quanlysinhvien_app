8571f8ffa498013e8c68ef6516c57bfc8b310c44
Vinh3 <101193855+vinh333@users.noreply.github.com>
2023-10-10 22:23:26 +0700

    add tinh nang sapxep theo masv trong danhsachsinhvien.xml

--- app/src/main/java/com/example/quanlysinhvien_app/Select/Select_Sinhvien.java
+++ app/src/main/java/com/example/quanlysinhvien_app/Select/Select_Sinhvien.java
@@ -1,15 +1,27 @@
 package com.example.quanlysinhvien_app.Select;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.quanlysinhvien_app.Adapter.SinhVienAdapter;
 import com.example.quanlysinhvien_app.DatabaseHelper;
 import com.example.quanlysinhvien_app.Database.SINHVIEN;
 import com.example.quanlysinhvien_app.R;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Select_Sinhvien extends AppCompatActivity {
+
+    private List<SINHVIEN> sinhVienList;
+    private SinhVienAdapter adapter;
+    private ListView listView;
+    private boolean isAscendingOrder = true; // Biến để theo dõi trạng thái sắp xếp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +30,42 @@ public class Select_Sinhvien extends AppCompatActivity {
 
         // Lấy danh sách sinh viên từ cơ sở dữ liệu hoặc nguồn dữ liệu khác
         DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        List<SINHVIEN> sinhVienList = databaseHelper.getAllSinhVien();
+        sinhVienList = databaseHelper.getAllSinhVien();
 
         // Tìm ListView bằng ID
-        ListView listView = findViewById(R.id.listView_Sinhvien);
+        listView = findViewById(R.id.listView_Sinhvien);
 
         // Tạo một instance của SinhVienAdapter và đặt adapter cho ListView
-        SinhVienAdapter adapter = new SinhVienAdapter(this, sinhVienList);
+        adapter = new SinhVienAdapter(this, sinhVienList);
         listView.setAdapter(adapter);
+
+        // Tìm nút lọc sinh viên theo tên và mã sinh viên bằng ID
+        TextView btnLocSV = findViewById(R.id.btn_locsv);
+        btnLocSV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Xử lý sự kiện khi nút lọc được nhấn
+                // Sắp xếp dữ liệu từ A đến Z hoặc từ Z đến A tùy vào giá trị của isAscendingOrder
+                Collections.sort(sinhVienList, new Comparator<SINHVIEN>() {
+                    @Override
+                    public int compare(SINHVIEN sv1, SINHVIEN sv2) {
+                        int result;
+                        if (isAscendingOrder) {
+                            result = sv1.getMaSV().compareToIgnoreCase(sv2.getMaSV());
+                        } else {
+                            result = sv2.getMaSV().compareToIgnoreCase(sv1.getMaSV());
+                        }
+                        return result;
+                    }
+                });
+
+                // Đảo ngược giá trị của biến isAscendingOrder để sắp xếp ngược lại ở lần tiếp theo
+                isAscendingOrder = !isAscendingOrder;
+
+                // Cập nhật dữ liệu trong adapter và hiển thị lại ListView
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
+
