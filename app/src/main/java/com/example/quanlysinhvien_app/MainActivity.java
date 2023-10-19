168a66565067ac3942b13ecf5d1779b28d52dba6
4nh <tranhanhle.2003@gmail.com>
2023-10-19 22:01:08 +0700

    mapinxong

--- app/src/main/java/com/example/quanlysinhvien_app/MainActivity.java
+++ app/src/main/java/com/example/quanlysinhvien_app/MainActivity.java
@@ -18,68 +18,69 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//
+//        PinManager pinManager = new PinManager(this);
+//        String savedPin = pinManager.getPin();
+//
+//        if (savedPin == null) {
+        setContentView(R.layout.activity_main);
 
-        PinManager pinManager = new PinManager(this);
-        String savedPin = pinManager.getPin();
-
-        if (savedPin == null) {
-
-            setContentView(R.layout.activity_main);
-            LinearLayout linearLayoutTrangChu = findViewById(R.id.LLnhapdiem);
-            LinearLayout linearLayoutDiemdanh = findViewById(R.id.LLdiemdanh);
-            LinearLayout linearLayoutCaidat = findViewById(R.id.LLcaidat);
-            Button btnSV = findViewById(R.id.btnsv);
-            Button btnKhoa = findViewById(R.id.btnkhoa);
+        LinearLayout linearLayoutTrangChu = findViewById(R.id.LLnhapdiem);
+        LinearLayout linearLayoutDiemdanh = findViewById(R.id.LLdiemdanh);
+        LinearLayout linearLayoutCaidat = findViewById(R.id.LLcaidat);
+        Button btnSV = findViewById(R.id.btnsv);
+        Button btnKhoa = findViewById(R.id.btnkhoa);
 
 
-            linearLayoutTrangChu.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Chuyển sang màn hình Trang Chủ
-                    Intent intent = new Intent(MainActivity.this, nhapdiem.class);
-                    startActivity(intent);
-                }
-            });
-            linearLayoutDiemdanh.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Chuyển sang màn hình Trang Chủ
-                    Intent intent = new Intent(MainActivity.this, diemdanh.class);
-                    startActivity(intent);
-                }
-            });
+        linearLayoutTrangChu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang màn hình Trang Chủ
+                Intent intent = new Intent(MainActivity.this, nhapdiem.class);
+                startActivity(intent);
+            }
+        });
+        linearLayoutDiemdanh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang màn hình Trang Chủ
+                Intent intent = new Intent(MainActivity.this, diemdanh.class);
+                startActivity(intent);
+            }
+        });
 
-            linearLayoutCaidat.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Chuyển sang màn hình Trang Chủ
-                    Intent intent = new Intent(MainActivity.this, chucnangthem.class);
-                    startActivity(intent);
-                }
-            });
-            btnSV.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Chuyển sang màn hình Trang Chủ
-                    Intent intent = new Intent(MainActivity.this, sinhvien.class);
-                    startActivity(intent);
-                }
-            });
+        linearLayoutCaidat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang màn hình Trang Chủ
+                Intent intent = new Intent(MainActivity.this, chucnangthem.class);
+                startActivity(intent);
+            }
+        });
+        btnSV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang màn hình Trang Chủ
+                Intent intent = new Intent(MainActivity.this, sinhvien.class);
+                startActivity(intent);
+            }
+        });
 
-            btnKhoa.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Chuyển sang màn hình Trang Chủ
-                    Intent intent = new Intent(MainActivity.this, dskhoa.class);
-                    startActivity(intent);
-                }
-            });
+        btnKhoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang màn hình Trang Chủ
+                Intent intent = new Intent(MainActivity.this, dskhoa.class);
+                startActivity(intent);
+            }
+        });
 
-        } else {
-            // Mã PIN đã cài đặt, chuyển đến trang nhập mã PIN
-            Intent intent = new Intent(this, PinEntryActivity.class);
-            startActivity(intent);
-
-        }
+//        } else {
+//            // Mã PIN đã cài đặt, chuyển đến trang nhập mã PIN
+//            Intent intent = new Intent(this, PinEntryActivity.class);
+//            startActivity(intent);
+//
+//        }
     }
+
 }
