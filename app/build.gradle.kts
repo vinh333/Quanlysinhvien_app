6b1e480c00522a9e6e02b21162f2aa7407d3dd3a
Vinh3 <101193855+vinh333@users.noreply.github.com>
2023-11-06 21:54:54 +0700

    [dang lam] Bieudo học sinh vắng

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.squareup.picasso:picasso:2.8")
-//    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,7 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
     }
-    repositories {
-        maven (  "https://jitpack.io" )
-    }
+
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
