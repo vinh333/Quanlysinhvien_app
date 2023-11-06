10c6b4980c8f699d0f01ee24fda57c4ef04a0cdd
Vinh3 <101193855+vinh333@users.noreply.github.com>
2023-11-06 21:45:17 +0700

    [dang lam] Bieudo học sinh vắng

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,13 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
     }
+    repositories {
+        maven (  "https://jitpack.io" )
+    }
 }
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
 }
+
