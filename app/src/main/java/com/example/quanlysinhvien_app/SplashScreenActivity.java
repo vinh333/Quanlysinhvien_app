19b1c2ced1370ddf1718f62ea7c33f3783495228
Vinh3 <101193855+vinh333@users.noreply.github.com>
2023-12-01 09:04:24 +0700

    [HOÀN THÀNH] Tính năng "Logo" và màn hình "SplashScreen"

---
+++ app/src/main/java/com/example/quanlysinhvien_app/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.quanlysinhvien_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // Delay for 3 seconds and then move to the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
+    }
+}
+
